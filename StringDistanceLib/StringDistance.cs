0cd0843e77cdfe2d3e1ea908fe34c50b8b3ce7e2
Joe <foxr4@foxjazz.net>
2016-01-05 22:21:45 -0700

    string distance

---
+++ StringDistanceLib/StringDistance.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringDistance
+{
+    public class StringDistance
+    {
+        public StringDistance(string input, IEnumerable<string> list)
+        {
+            dl = new List<larray>();
+            foreach (var l in list)
+            {
+                var la = new larray { s = l, d = Compute(input, l) };
+                dl.Add(la);
+            }
+            Results = dl.OrderBy(m => m.d).Select(m => m.s);
+        }
+        struct larray
+        {
+            public string s;
+            public int d;
+        }
+        List<larray> dl;
+
+        public IEnumerable<string> Results { get; set; }
+
+
+        public static int Compute(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
+        }
+    }
+
+}
+
---
+++ TestStringDistance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestStringDistance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> x;
+            x = new List<string>();
+            x.Add("test2");
+            x.Add("test");
+            var sd = new StringDistance.StringDistance("tes", x);
+            foreach(string s in sd.Results)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
