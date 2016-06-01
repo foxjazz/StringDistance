355eccb3fdeebdd31d1c8838523802d6fb636378
Joe <foxr4@foxjazz.net>
2016-06-01 17:49:29 -0600

    added typescript solution

---
+++ NodejsConsoleApp1/stringdistance.ts
@@ -0,0 +1,105 @@
+﻿export interface Istringdistance {
+    prop: string;
+    list: Array<Object>;
+    input: string;
+
+}
+interface Iobj {
+    distance: number;
+    o: Object;
+}
+
+//interface Iorderdlist {
+//    olist: Array<Iobj>;
+
+//}
+class tobeOrderedList {
+    olist: Array<Iobj>;
+}
+class orderedList {
+    olist: Array<Iobj>;
+}
+export class stringdistance{
+
+    public result: Array<Object>;
+    constructor(sdinput: Istringdistance) { this.orderList(sdinput); }
+
+    private indexPropByPath(object: any, path: string) {
+        let result = object;
+        for (const portion of path.split(".")) {
+            result = result[portion];
+        }
+        return result;
+    }
+
+    private orderList(data: Istringdistance) {
+        let arr: Array<Object>;
+        arr = data.list;
+        if (arr == null || arr.length == 0 || !arr[0].hasOwnProperty(data.prop)) {
+            throw ('invalid data');
+        }
+        let n: number;
+        let tobeo = new tobeOrderedList();
+        let i: number;
+        
+        for (i = 0; i < arr.length; i++)
+        {
+            let sresult = this.indexPropByPath(arr[i], data.prop);
+
+            let nd: Iobj = {distance: this.distance(data.input, sresult), o: arr[i]};
+            tobeo.olist.push(nd);
+        }
+        this.result = tobeo.olist.sort((a, b) => {
+            if (a.distance < b.distance) return -1;
+            if (a.distance > b.distance) return 1;
+            return 0;
+        });
+        //Here we need to order the list and assign it to result.
+    }
+
+    private distance(s1: string, s2: string): number {
+    
+    /* return max value of data type Number on error */
+    if (!s1 || !s2) {
+        return Number.MAX_VALUE;
+    }
+
+    let m: number = s1.length;
+    let n: number = s2.length;
+    let i: number;
+    let j: number;
+    let cost: number = 0;
+    let matrix: Array<number>[] = new Array();
+
+    /*
+     * for all i and j, matrix[i][j] holds the edit distance between the
+     * first i characters of s and the first j characters of t.
+     *
+     * @note
+     * Array has (m + 1) * (n + 1) values
+     */
+    for (i = 0; i <= m; i++) {
+        matrix[i] = new Array();
+        matrix[i][0] = i;
+    }
+    for (j = 0; j <= n; j++) {
+        matrix[0][j] = j;
+    }
+
+    /* determine longest common substring sequence */
+    for (j = 1; j <= n; j++) {
+        for (i = 1; i <= m; i++) {
+            /* subtract one to start, at zero index */
+            cost = (s1.charAt(i - 1) == s2.charAt(j - 1)) ? 0 : 1;
+
+            /* insert, delete, substitute */
+            matrix[i][j] = Math.min(matrix[i][j - 1] + 1,
+                Math.min(matrix[i - 1][j] + 1,
+                    matrix[i - 1][j - 1] + cost));
+        }
+    }
+
+    /* return edit distance */
+    return matrix[m][n];
+}
+}
---
+++ NodejsConsoleApp1/testsd.ts
@@ -0,0 +1,31 @@
+﻿import {stringdistance, Istringdistance} from './stringdistance.ts';
+import {ItemTypesService} from './itemtypes';
+
+export interface ItemTypesA { items: Array<ItemType>; }
+export interface ItemTypes { totalCount_str: string; pageCount: number; items: Array<ItemType>; }
+export interface ItemType { marketGroup: MarketGroup; type: Type; id: number; id_str: string; }
+
+export interface MarketGroup { href: string; id: number; id_str: string; }
+export interface Type { id_str: string; href: string; id: number; name: string; icon: Icon; }
+export interface Icon { href: string; }
+
+export const ItemTypeDescriptor = {
+    id: Number, id_str: String,
+    marketGroup: { href: String, id: Number, id_str: String, },
+    Type: { id_str: String, href: String, id: Number, name: String }
+}
+
+
+class TestStringDistance {
+    public result: Array<Object>;
+    public testsd(inp: string) {
+        let xx = new ItemTypesService();
+
+        let obj: Istringdistance = {
+            prop: 'item.name', list: xx.getItemTypes(), input: inp
+        };
+        let sd = new stringdistance(obj);
+        this.result = sd.result;
+
+    }
+}
